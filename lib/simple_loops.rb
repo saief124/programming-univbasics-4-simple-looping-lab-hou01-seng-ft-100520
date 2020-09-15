4c4fb634cc5950ecba99a1c7a62961dd7c99b3be
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-15 01:35:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,28 @@ def loop_message_five_times(string)
     counter +=1
   end
 end
+
+def loop_message_n_times(string, n)
+  counter=0
+  while counter < n do
+    puts string
+    counter +=1
+  end
+end
+
+def output_array(array)
+  counter =0
+  while counter<length.array do
+    puts array[counter]
+    counter +=1
+  end
+end
+
+def return_string_array (array)
+  counter=0
+  while counter<length.array do
+  string_array=array[counter].to_s
+  counter +=1  
+  end
+  string_array
+end
